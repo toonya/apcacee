2ebf0c72cea03ee0d9dbf6bc43971bb3a4b93777
toonya <msc5762@me.com>
2013-05-22 14:18:24 +0800

    Add custom options page in functions.php

--- wp-content/themes/conf/functions.php
+++ wp-content/themes/conf/functions.php
@@ -98,10 +98,10 @@ function twentyeleven_setup() {
 	add_theme_support( 'automatic-feed-links' );
 
 	// This theme uses wp_nav_menu() in one location.
-	register_nav_menu( 'primary', __( 'Primary Menu', 'twentyeleven' ) );
+/* 	register_nav_menu( 'primary', __( 'Primary Menu', 'twentyeleven' ) ); */
 
 	// Add support for a variety of post formats
-	add_theme_support( 'post-formats', array( 'aside', 'link', 'gallery', 'status', 'quote', 'image' ) );
+/* 	add_theme_support( 'post-formats', array( 'aside', 'link', 'gallery', 'status', 'quote', 'image' ) ); */
 
 	$theme_options = twentyeleven_get_theme_options();
 	if ( 'dark' == $theme_options['color_scheme'] )
@@ -664,4 +664,108 @@ if ( is_admin() ) {
 
 <?php include_once('conf_detail.php'); ?>
 <?php include_once('section-page/section-page.php'); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+	add_action( 'admin_menu', 'my_admin_menu' );
+	function my_admin_menu() {
+	    add_menu_page( 'My Theme Options', 'My Theme Options', 'edit_theme_options', 'my-theme-options', 'my_theme_options' );
+	}
+	
+	function my_theme_options() {
+	?>
+	    <div class="wrap">
+	        <div><br></div>
+	        <h2>My Theme Options</h2>
+	
+	        <form method="post" action="options.php">
+	            <?php wp_nonce_field( 'update-options' ); ?>
+	            <?php settings_fields( 'my-options' ); ?>
+	            <?php do_settings_sections( 'my-options' ); ?>
+	            <?php submit_button(); ?>
+	        </form>
+	    </div>
+	<?php
+	}
+	
+	
+	add_action( 'admin_init', 'my_register_admin_settings' );
+	function my_register_admin_settings() {
+	    register_setting( 'my-options', 'my-options' );
+	
+	    // Settings fields and sections
+	    add_settings_section( 'section_typography', 'Typography Options', 'my_section_typography', 'my-options' );
+	    add_settings_field( 'primary-font', 'Primary Font', 'my_field_primary_font', 'my-options', 'section_typography' );
+	    add_settings_field( 'sub-font', 'Sub Font', 'my_field_sub_font', 'my-options', 'section_typography' );
+	}
+	function my_section_typography() {
+	    echo 'Section description can go here.';
+	}
+	
+	function my_field_primary_font() {
+	    $options = (array) get_option( 'my-options' );
+	    $fonts = get_my_available_fonts();
+	    $current_font = 'arial';
+	
+	    if ( isset( $options['primary-font'] ) )
+	        $current_font = $options['primary-font'];
+	
+	    ?>
+	        <select name="my-options[primary-font]">
+	        <?php foreach( $fonts as $font_key => $font ): ?>
+	            <option <?php selected( $font_key == $current_font ); ?> value="<?php echo $font_key; ?>"><?php echo $font['name']; ?></option>
+	        <?php endforeach; ?>
+	        </select>
+	    <?php
+	}
+	function my_field_sub_font() {
+	    $options = (array) get_option( 'my-options' );
+	    $fonts = get_my_available_fonts();
+	    $current_font = 'arial';
+	
+	    if ( isset( $options['sub-font'] ) )
+	        $current_font = $options['sub-font'];
+	
+	    ?>
+	        <select name="my-options[sub-font]">
+	        <?php foreach( $fonts as $font_key => $font ): ?>
+	            <option <?php selected( $font_key == $current_font ); ?> value="<?php echo $font_key; ?>"><?php echo $font['name']; ?></option>
+	        <?php endforeach; ?>
+	        </select>
+	    <?php
+	}
+	function get_my_available_fonts() {
+	    $fonts = array(
+	        'open-sans' => array(
+	            'name' => 'Open Sans',
+	            'import' => '@import url(http://fonts.googleapis.com/css?family=Open+Sans);',
+	            'css' => "font-family: 'Open Sans', sans-serif;"
+	        ),
+	        'lato' => array(
+	            'name' => 'Lato',
+	            'import' => '@import url(http://fonts.googleapis.com/css?family=Lato);',
+	            'css' => "font-family: 'Lato', sans-serif;"
+	        ),
+	        'arial' => array(
+	            'name' => 'Arial',
+	            'import' => '',
+	            'css' => "font-family: Arial, sans-serif;"
+	        )
+	    );
+	
+	    return apply_filters( 'my_available_fonts', $fonts );
+	}
+	?>
+	
 
